958d845b4821af566c147e5eca81361662e75cd7
git config --global user.name <aibolit999999999@gmail.com>
2022-02-23 15:42:40 +0300

    fixadd function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let brackets = bracketsConfig.join('').replace(/,/g, '');
+  let stack = [];
+for (let bracket of str) {
+      let braketsBra = brackets.indexOf(bracket)
+
+      if (braketsBra % 2 === 0) {
+          stack.push(braketsBra)
+          if (bracket === brackets[braketsBra+1]){
+              stack.pop();
+          }
+
+      }
+      else {
+          if (stack.pop() !== braketsBra-1){
+              return false;
+          }
+      }
+
+  }
+  return stack.length === 0
 }
