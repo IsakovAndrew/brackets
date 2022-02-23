5b7aceee9eb9dc2ed2b616b783778bedb8f580aa
git config --global user.name <aibolit999999999@gmail.com>
2022-02-23 21:23:34 +0300

    fix:function

--- src/index.js
+++ src/index.js
@@ -1,22 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  let brackets = bracketsConfig.join('').replace(/,/g, '');
-  let stack = [];
+let brackets = bracketsConfig.join('').replace(/,/g, '');
+let stack = [];
 for (let bracket of str) {
-      let braketsBra = brackets.indexOf(bracket)
+    let braketsBra = brackets.indexOf(bracket)
 
-      if (braketsBra % 2 === 0) {
-          stack.push(braketsBra)
-          if (bracket === brackets[braketsBra+1]){
-              stack.pop();
-          }
+    if (braketsBra % 2 === 0) {
+        stack.push(braketsBra)
+        if (bracket === brackets[braketsBra+1]){
+            stack.pop();
+        }
+    }
+    else {
+        if (stack.pop() !== braketsBra-1){
+            return false;
+        }
+    }
+}
+return stack.length === 0
 
-      }
-      else {
-          if (stack.pop() !== braketsBra-1){
-              return false;
-          }
-      }
-
-  }
-  return stack.length === 0
 }
